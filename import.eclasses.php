5e8d4782dc1492d45aeaf907a84f3e1261a01fec
Steve Dibb <steve.dibb@gmail.com>
2012-12-28 11:20:10 -0700

    Reset eclass sequence if table is empty

--- import.eclasses.php
+++ import.eclasses.php
@@ -11,6 +11,14 @@
 	$arr = $tree->getEclasses();
 	
 	$arr_diff = importDiff($table, $arr);
+
+	// Reset sequence if table is empty
+	$sql = "SELECT COUNT(1) FROM eclass;";
+	$count = $db->getOne($sql);
+	if($count == 0) {
+		$sql = "ALTER SEQUENCE eclass_id_seq RESTART WITH 1;";
+		$db->query($sql);
+	}
 	
 	if(count($arr_diff['delete'])) {
 		foreach($arr_diff['delete'] as $name) {
