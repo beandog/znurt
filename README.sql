57776a07ad1b418e0c3e8e29e320fa6d55b24816
Steve Dibb <steve.dibb@gmail.com>
2012-12-28 12:23:57 -0700

    Add notes about using TRUNCATE TABLE

--- README.sql
+++ README.sql
@@ -1,2 +1,6 @@
 Create an SQL file to drop the database and recreate it:
 pg_dump -C -x -s -O -f portage.sql
+
+Quickly flush (remove all records) from a table.  If there are foreign keys, delete the children as well.  Using TRUNCATE is exponentially faster than DELETE.
+
+TRUNCATE TABLE table_name CASCADE DELETE;
