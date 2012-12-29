ecb46563c9407db639c4021be7feaf802c2f8fe9
Steve Dibb <steve.dibb@gmail.com>
2012-12-28 20:04:27 -0700

    PDO include

---
+++ header.pdo.php
@@ -0,0 +1,19 @@
+<?php
+
+	/*
+	 * Create a PDO instance to connect to the PostgreSQL database
+	 */
+
+	// Specific connection settings
+	require_once 'header.dsn.php';
+
+	try {
+		$dbh = new PDO($pdo_dsn);
+	} catch(PDOException $e) {
+		echo 'PDO connection failed: '.$e->getMessage();
+		echo "\n";
+	}
+
+	$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+?>
