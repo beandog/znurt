99d6e69375bcd318d15ee9d122b248b0ac90b57e
Steve Dibb <steve.dibb@gmail.com>
2014-02-18 03:54:07 +0000

    Drop support for importing bugzilla bugs

--- import.all.php
+++ import.all.php
@@ -39,7 +39,7 @@
 	// Thankfully, I've never really had these break down much, never
 	// had much use for the grouping.
 	$arr_import['base'] = array('arches', 'licenses');
-	$arr_import['packages'] = array('categories', 'packages', 'bugzilla');
+	$arr_import['packages'] = array('categories', 'packages');
 	$arr_import['ebuilds'] = array('ebuilds');
 	$arr_import['metadata'] = array('ebuild_metadata', 'ebuild_arch', 'ebuild_homepage', 'ebuild_license', 'package_mask', 'ebuild_mask', 'ebuild_ev', 'use_global', 'use_local', 'use_expand', 'ebuild_use');
 	$arr_import['final'] = array('final');
