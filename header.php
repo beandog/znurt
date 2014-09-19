30fb26aec11439080b0b1e866a4c1da50adccc03
Steve Dibb <steve.dibb@gmail.com>
2014-09-19 16:56:10 -0600

    Update error logging level

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 
-	error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+	error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE);
 
 	// Include MDB2 credentials
 	// See header.mdb2.php for generic connection settings
@@ -22,27 +22,27 @@
 		require_once 'class.common.php';
 		require_once 'class.shell.php';
 	}
-	
+
 	$now = $db->getOne("SELECT NOW();");
-	
+
 	function importDiff($table, $arr_new, $where = "") {
- 		
+
  		$db =& MDB2::singleton();
- 		
+
  		if($where)
  			$where = "WHERE $where";
- 		
+
  		$sql = "SELECT name FROM $table $where ORDER BY name;";
  		$arr_old = $db->getCol($sql);
- 		
+
  		$arr_insert = array_diff($arr_new, $arr_old);
  		$arr_delete = array_diff($arr_old, $arr_new);
- 		
+
  		$arr = array('insert' => $arr_insert, 'delete' => $arr_delete);
- 		
+
  		return($arr);
  	}
- 	
+
  	// This gets used everywhere, might as well create it here
 	// and check for it later.
 	require_once 'class.portage.tree.php';
