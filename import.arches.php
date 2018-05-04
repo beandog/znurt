c0a2697e00f09dc14bee5580e8f774cf9505943c
Steve Dibb <steve.dibb@gmail.com>
2018-05-03 20:19:33 -0600

    Don't display query

--- import.arches.php
+++ import.arches.php
@@ -26,7 +26,6 @@
 	if(count($arr['delete'])) {
 		foreach($arr['delete'] as $name) {
 			$sql = "DELETE FROM arch WHERE name = ".$db->quote($name).";";
-			shell::msg($sql);
 			$db->query($sql);
 		}
 	}
