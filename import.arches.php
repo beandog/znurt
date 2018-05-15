31e001f961db884bde44970bddee6ecfdb7ab73e
Steve Dibb <steve.dibb@gmail.com>
2018-05-14 18:56:46 -0600

    Cleanup arch import

--- import.arches.php
+++ import.arches.php
@@ -8,35 +8,49 @@
 		$tree =& PortageTree::singleton();
 	}
 
-	// FIXME This is really dumb, just grab all the arches
-	// since I look at all of them now.
-	$arr_arches = $tree->getArches();
-	$arr_arches = array_merge($arr_arches, $tree->getArches(true));
+	// Get and display Portage's arches
+	$a_tree_arches = $tree->getArches(true);
+	$d_tree_arches = implode(' ', $a_tree_arches);
+	echo "* Larry:	$d_tree_arches\n";
 
-	$arr = importDiff('arch', $arr_arches);
+	// Display Znurt's arches
+	$sql = "SELECT name FROM arch ORDER BY name;";
+	$a_znurt_arches = pg_column_array(pg_fetch_all(pg_query($sql)));
+	$d_znurt_arches = implode(" ", $a_znurt_arches);
+	$i_znurt_arches = count($a_znurt_arches);
+	echo "* Znurt:	$d_znurt_arches\n";
+
+	// Get the difference between the two sets and display changes
+	$a_import_diff = importDiff('arch', $a_tree_arches);
+	$i_insert_count = count($a_import_diff['insert']);
+	if($i_insert_count) {
+		$d_insert_arch = implode(' ', $a_import_diff['insert']);
+		echo "* Insert: $d_insert_arch\n";
+	}
+	$i_delete_count = count($a_import_diff['delete']);
+	if($i_delete_count) {
+		$d_delete_arch = implode(' ', $a_import_diff['delete']);
+		echo "* Delete: $d_delete_arch\n";
+	}
 
 	// Reset sequence if table is empty
-	$sql = "SELECT COUNT(1) FROM arch;";
-	$count = $db->getOne($sql);
-	if($count == 0) {
+	if(!$i_znurt_arches) {
 		$sql = "ALTER SEQUENCE arch_id_seq RESTART WITH 1;";
-		$db->query($sql);
+		pg_query($sql);
 	}
 
-	if(count($arr['delete'])) {
-		foreach($arr['delete'] as $name) {
-			$sql = "DELETE FROM arch WHERE name = ".$db->quote($name).";";
-			$db->query($sql);
-		}
+	// Delete removed arches
+	foreach($a_import_diff['delete'] as $arch) {
+		$q_arch = pg_escape_literal($arch);
+		$sql = "DELETE FROM arch WHERE name = $q_arch;";
+		pg_query($sql);
 	}
 
-	if(count($arr['insert'])) {
-		foreach($arr['insert'] as $name) {
-			$arr_insert = array('name' => $name);
-			$db->autoExecute('arch', $arr_insert, MDB2_AUTOQUERY_INSERT);
-		}
+	// Insert new arches
+	foreach($a_import_diff['insert'] as $arch) {
+		$q_arch = pg_escape_literal($arch);
+		$sql = "INSERT INTO arch (name) VALUES ($q_arch);";
+		pg_query($sql);
 	}
-
-	unset($arr);
 
 ?>
