31848edb78ecf310e87cc677b03fa39ab0d6891d
Steve Dibb <steve.dibb@gmail.com>
2018-05-16 18:38:10 -0600

    Remove comments that would apply to bugs in portage ebuild class

--- import.ebuilds.php
+++ import.ebuilds.php
@@ -184,40 +184,6 @@
 			echo "\n";
 		}
 
-
-						/*
-						// Caught something my regexp can't find -- that is, recompiling the filename from the parsed values didn't work because the file doesn't exist
-						if($source = '') {
-							echo "FIXME - couldn't find ebuild filename\n";
-							print_r($arr);
-							echo "\n";
-							$fixme = true;
-						}
-
-						if(!$e->filesize) {
-							echo "FIXME - empty filesize for ebuild -- non-existant?\n";
-							print_r($arr);
-							echo "\n";
-							$fixme = true;
-						}
-
-						if(!$e->hash) {
-							echo "FIXME - empty hash for ebuild -- non-existant?\n";
-							print_r($arr);
-							echo "\n";
-							$fixme = true;
-						}
-
-						if($fixme)
-							continue;
-
-						$rs = pg_execute('insert_ebuild', array_values($arr));
-						if($rs === false) {
-							echo pg_last_error()."\n";
-							echo "\n";
-						}
-						*/
-
 	}
 
 	if($i_update_count || $i_insert_count)
