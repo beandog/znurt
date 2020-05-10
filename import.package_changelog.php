56815650f8966542b94d5f08b94ee19cbdf460a7
Steve Dibb <steve.dibb@gmail.com>
2020-05-10 05:18:39 -0600

    exec() builds on previous array, so clear out; switch directory back to original when import is finished

--- import.package_changelog.php
+++ import.package_changelog.php
@@ -16,19 +16,31 @@
 	$sql = "SELECT p.id, c.name || '/' || p.name AS cp FROM package p JOIN category c ON p.category = c.id LEFT OUTER JOIN package_changelog pc ON pc.package = p.id WHERE pc.changelog IS NULL ORDER BY c.name, p.name;";
 	$rs = pg_query($sql);
 
+	$dir = getcwd();
+
+	chdir($git_dir);
+
 	while($row = pg_fetch_assoc($rs)) {
 
 		$id = $row['id'];
 		$cp = $row['cp'];
 
+		// This shouldn't happen
+		if(!is_dir("$git_dir/$cp"))
+			continue;
+
 		echo "Import changelog: $cp\n";
 
-		$cmd = "git log $git_dir/$cp";
-		chdir("$git_dir/$cp");
+		$cmd = "git log $cp";
+
+		// exec() appends to existing array, so it has to be cleared out first
+		$output = array();
+
 		exec($cmd, $output, $retval);
-		$changelog = implode("\n", $output);
 
 		if($retval == 0 && $changelog) {
+
+			$changelog = implode("\n", $output);
 
 			$q_changelog = pg_escape_literal($changelog);
 			$sql = "INSERT INTO package_changelog (package, changelog) VALUES ($id, $q_changelog);";
@@ -37,5 +49,7 @@
 		}
 
 	}
+
+	chdir($dir);
 
 ?>
